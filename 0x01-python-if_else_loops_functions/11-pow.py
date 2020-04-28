eb6add5eda168d125f96cb5a37cec22ed6ab7be9
Edison Giraldo A <1470@holbertonschool.com>
2020-04-28 17:22:24 +0000

    computes a to the power of b and return the value.

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def pow(a, b):
+    a_b = 1
+    while b != 0:
+        if b > 0:
+            a_b *= a
+            b -= 1
+        if b < 0:
+            a_b *= 1 / a
+            b += 1
+    return(a_b)
