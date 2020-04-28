5205844c0546d9ef624dd358aa362e7b4dc1fb5f
Edison Giraldo A <1470@holbertonschool.com>
2020-04-28 17:54:37 +0000

    prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter != 101 and letter != 113:  # ASCII code for 'e' and 'q'
-        print("{:s}".format(chr(letter)), end='')
+    if letter != 101:
+        if letter != 113:  # ASCII codes for 'e' and 'q'
+            print("{:s}".format(chr(letter)), end='')
