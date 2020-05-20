e1101bfde8229a5094a4295ad794fca276963f40
Edison Giraldo A <1470@holbertonschool.com>
2020-05-20 01:14:59 +0000

    Adding position attribute and setter and getter properties

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,7 +14,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        if type(position) != tuple or position[0] < 0 or position[1] < 0:
+        if (type(position) != tuple or type(position[0]) != int or
+           type(position[1]) != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
@@ -44,7 +47,10 @@ class Square:
     @position.setter
     def position(self, value):
         """property setter method for set position attribute."""
-        if type(value) != tuple or position[0] < 0 or position[1] < 0:
+        if (type(position) != tuple or type(position[0]) != int or
+           type(position[1]) != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
