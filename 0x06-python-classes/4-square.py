d306eab44c513bf70a109ba07ab31ed50163411b
Edison Giraldo A <1470@holbertonschool.com>
2020-05-20 00:15:11 +0000

    Implementing setter and getter properties for size attribute of class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,10 +8,6 @@ class Square:
     def __init__(self, size=0):
         """Initialize size attribute of instance
            the size has to be >= to 0."""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
@@ -25,5 +21,9 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         """property setter method for set size attribute."""
         self.__size = value
