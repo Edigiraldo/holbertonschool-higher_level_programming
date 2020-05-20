131893b168685ff3071c6efca8c05209bb66694a
Edison Giraldo A <1470@holbertonschool.com>
2020-05-20 00:41:21 +0000

    implementing my_print method that prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module implements the Square class."""
+
+
+class Square:
+    """Class Square."""
+
+    def __init__(self, size=0):
+        """Initialize size attribute of instance
+           the size has to be >= to 0."""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This method returns the area of the square object."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """property that returns the value of size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter method for set size attribute."""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        size = self.__size
+        squares = size
+        while size != 0:
+            for i in range(squares):
+                print('#', end='')
+            size -= 1
+            print()
+        print()
