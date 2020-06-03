c899029df612f5c88f2f5e6c4756584d6c5b4089
Edison Giraldo A <1470@holbertonschool.com>
2020-06-03 02:07:48 +0000

    Implementing area and __str__ methods

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """"Module for Rectangle class."""
-
-
-class BaseGeometry:
-    """BaseGeometry class."""
-    def area(self):
-        """raise a exception."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method that validates value. It has to be an
-        integer greater than zero."""
-        if type(value) != int:
-            raise Exception("{} must be an integer".format(name))
-        elif value <= 0:
-            raise Exception("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
