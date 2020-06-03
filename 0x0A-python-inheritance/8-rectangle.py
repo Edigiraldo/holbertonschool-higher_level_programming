9174ae9c68e61ea801ee59e74dcbcc911f99fe10
Edison Giraldo A <1470@holbertonschool.com>
2020-06-03 01:10:23 +0000

    Implementing rectangle class. Subclass of BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,12 +2,13 @@
 """"Module for Rectangle class."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class that inherits from BaseGeometry."""
     def __init__(self, width, height):
         """Init method for Rectangle class.
         initialize width and height."""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
