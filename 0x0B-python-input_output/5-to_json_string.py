649d09d8e682676bd899498d77a5c2beda2bde6e
Edison Giraldo A <1470@holbertonschool.com>
2020-06-04 02:34:05 +0000

    function that returns the JSON representation of an object (string)

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -2,7 +2,8 @@
 """module for to_json_string function"""
 import json
 
+
 def to_json_string(my_obj):
-    """function that returns the JSON 
+    """function that returns the JSON
     representation of an object (string)"""
     return json.dumps(my_obj)
