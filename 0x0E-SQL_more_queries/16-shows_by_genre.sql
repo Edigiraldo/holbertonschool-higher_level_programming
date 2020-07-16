8789593bea8d09a2b89cb3152811d3bb0cd42d52
Edison Giraldo A <1470@holbertonschool.com>
2020-07-16 04:05:41 +0000

    script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT title, name FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY title, name ASC;
