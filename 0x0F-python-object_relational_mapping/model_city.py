7a4a0cb8be7a4107d65217955ea54f8cbf374770
Edison Girado A <1470@Holbertonschool.com>
2020-08-09 23:07:23 -0500

    model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -16,4 +16,4 @@ class City(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
     state_id = Column(Integer, ForeignKey('states.id'))
-    parent = relationship("State", back_populates="children")
+    states = relationship("State")
