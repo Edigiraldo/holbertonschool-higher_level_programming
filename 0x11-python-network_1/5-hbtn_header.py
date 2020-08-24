92a8416e9ef8f0e0d482a821d22eba83a981098a
Edison Girado A <1470@Holbertonschool.com>
2020-08-24 18:36:46 -0500

    Response header value - X-Request-Id.

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Response header value - X-Request-Id."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    var = response.headers['X-Request-Id']
+    print(var)
