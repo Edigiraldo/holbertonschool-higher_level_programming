a39d7694188bb3d998db66e01b4b50976025c4cb
Edison Girado A <1470@Holbertonschool.com>
2020-08-24 18:10:59 -0500

    Handling HTTP errors.

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Handling HTTP errors."""
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
