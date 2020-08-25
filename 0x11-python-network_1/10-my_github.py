6ea0949767b87a366db67f8fe463dd0a153626fd
Edison Girado A <1470@Holbertonschool.com>
2020-08-24 20:18:11 -0500

    github autentication.

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    passwd = argv[2]
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(user, passwd))
+    dict = response.json()
+    print(dict.get('id'))
