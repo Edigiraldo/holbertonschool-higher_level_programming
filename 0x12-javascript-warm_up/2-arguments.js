9f6338dfc1eb655a8bd9058fe7dd720130238bc5
Edison Girado A <1470@Holbertonschool.com>
2020-09-15 15:54:37 -0500

    if-else statement.

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const length = process.argv.length;
+let message = '';
+
+if (length === 2) {
+  message = 'No argument';
+} else if (length === 3) {
+  message = 'Argument found';
+} else {
+  message = 'Arguments found';
+}
+
+console.log(message);
