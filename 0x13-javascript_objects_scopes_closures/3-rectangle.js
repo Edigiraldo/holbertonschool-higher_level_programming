0f12cce8307ca4a0960d3ab19a878f937c882b24
Edison Girado A <1470@Holbertonschool.com>
2020-09-21 16:51:43 -0500

    Adding print instance method.

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,12 +9,12 @@ class Rectangle {
   }
 
   print () {
-    let str = ''
+    let str = '';
     for (let j = 0; j < this.width; j++) {
-      str += 'X'
+      str += 'X';
     }
     for (let i = 0; i < this.height; i++) {
-        console.log(str)
+      console.log(str);
     }
   }
 }
