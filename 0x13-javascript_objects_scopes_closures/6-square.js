2e9cd12617e2aa5b818a8e915f34d99c8bc1ddd4
Edison Girado A <1470@Holbertonschool.com>
2020-09-21 18:17:47 -0500

    Create Square class inherits from another equal class.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,40 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const SquareParent = require('./5-square');
 
-  print () {
-    let str = '';
-    for (let j = 0; j < this.width; j++) {
-      str += 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(str);
-    }
-  }
-
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
-
-  double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareParent {
   charPrint (c) {
     let charac = c;
     let str = '';
@@ -51,5 +19,4 @@ class Square extends Rectangle {
     }
   }
 }
-
 module.exports = Square;
