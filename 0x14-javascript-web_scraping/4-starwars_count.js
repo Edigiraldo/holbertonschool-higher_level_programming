fa29743e6b9cf997bcb602ab30f073b619e87427
Edison Girado A <1470@Holbertonschool.com>
2020-09-23 00:14:40 -0500

    number of movies where the character Wedge Antilles is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,11 @@ const request = require('request');
 const url = process.argv[2];
 const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
 request(url, function (error, response, body) {
+  let counter = 0;
   if (error) {
     console.log(error);
   } else {
     const results = JSON.parse(body).results;
-    let counter = 0;
     for (let i = 0; i < results.length; i++) {
       if (results[i].characters.includes(wedge) === true) {
         counter += 1;
